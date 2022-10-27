d52a68c2a5136ddb4fccb75559dc0c8a95a2baaf
Cristina SG <cristinasanchezg@gmail.com>
2022-10-26 21:53:01 -0500

    iniciorouter

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,26 @@
+import {Navigate, Route, Routes} from 'react-router-dom';
+
+
+
+function AppRouter () {
+    return (  
+        <>
+        <Routes>
+            <Route path='inicio' element={<Inicio/>}/>
+
+
+        </Routes>
+        
+        
+        </>
+    );
+
+
+
+    
+}
+
+export default AppRouter
+
+
+;
