6198bd9e5ddbd70e0dfa10de12a1aa627a6e5162
Cristina SG <cristinasanchezg@gmail.com>
2022-10-27 15:21:01 -0500

    intento2

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -1,9 +1,13 @@
+import dos from '../components/imagenes/dos.jpeg';
+
+
 function MainTitle() {
     return (  
         <>
         <div className="w3-container w3-padding-64" style={{maxHeight:350}} ></div>
-         <div className="w3-display-middle w3-center">
-            <img src="./components/imagenes/portada.jpeg" alt="portada" />
+         <div className=" w3-center">
+         <img src={dos} style={{ width: '100%', maxWidth: 1000 }} className="w3-margin-top" alt="dos" />
+            {/* <img src="./components/imagenes/portada.jpeg" alt="portada" /> */}
         <span className="w3-text-black" style={{ fontSize: 90 }}><br />KEEP&BITE</span>
       </div>
         
