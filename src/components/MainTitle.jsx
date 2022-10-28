afb3c0f96f3111b2fcd0f68e9d16a885cbcfbb60
Cristina SG <cristinasanchezg@gmail.com>
2022-10-28 15:59:29 -0500

    correcciones_import

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -1,31 +1,31 @@
-import PropTypes from 'prop-types';
-import dos from '../components/imagenes/dos.jpeg';
+// import PropTypes from 'prop-types';
+// import dos from '../components/imagenes/dos.jpeg';
 
 
 
-function MainTitle(props) {
-    return (
-        <>
-            <div className="w3-container w3-padding-64" style={{ maxHeight: 350 }} ></div>
-            <div className=" w3-center">
-                <img src={dos} style={{ width: '100%', maxWidth: 1000 }} className="w3-margin-top" alt="dos" />
-                {/* <img src="./components/imagenes/portada.jpeg" alt="portada" /> */}
-                <link className="w3-text-black" style={{ fontSize: 90 }} to="/Home">KEEP & BITE</link>
-                {/* <span className="w3-text-black" style={{ fontSize: 90 }}><br />KEEP&BITE</span> */}
-            </div>
+// function MainTitle(props) {
+//     return (
+//         <>
+//             <div className="w3-container w3-padding-64" style={{ maxHeight: 350 }} ></div>
+//             <div className=" w3-center">
+//                 <img src={dos} style={{ width: '100%', maxWidth: 1000 }} className="w3-margin-top" alt="dos" />
+//                 {/* <img src="./components/imagenes/portada.jpeg" alt="portada" /> */}
+//                 <link className="w3-text-black" style={{ fontSize: 90 }} to="/Home">KEEP & BITE</link>
+//                 {/* <span className="w3-text-black" style={{ fontSize: 90 }}><br />KEEP&BITE</span> */}
+//             </div>
 
-        </>
+//         </>
 
 
-    );
-}
+//     );
+// }
 
-MainTitle.PropTypes = {
-    title: PropTypes.string.isRequired,
-};
+// MainTitle.PropTypes = {
+//     title: PropTypes.string.isRequired,
+// };
 
-MainTitle.defaultProps = {
-    title: "Blog Default",
-}
+// MainTitle.defaultProps = {
+//     title: "Blog Default",
+// }
 
-export default MainTitle;
+// export default MainTitle;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
-
+import React from "react";
 import Home from '../pages/Home';
 import Donde from '../pages/Donde';
-import Acerca from '../pages/Acerca';
+
 import Menu from '../pages/Menu';
 import Reservas from '../pages/Reservas';
 import { Navigate, Route, Routes } from 'react-router-dom';
@@ -10,9 +10,9 @@ function AppRouter() {
     return (
         <>
             <Routes>
-                <Route path="home" element={<Home />} />
+                <Route path="/home" element={<Home />} />
                 <Route path="donde" element={<Donde />} />
-                <Route path="acerca" element={<Acerca />} />
+                {/* <Route path="acerca" element={<Acerca />} /> */}
                 <Route path="menu" element={<Menu />} />
                 <Route path="reservas" element={<Reservas />} />
 
